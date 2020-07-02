5ab6cb1067f32890ce7f87728d7240af1d95c028
cuip <yatri1609>
2020-07-02 09:35:24 -0400

    Editing domain Completed domain part of the hw3. Now we can access domain name as a whole

--- hw3/create_data.php
+++ hw3/create_data.php
@@ -9,27 +9,34 @@
    <?PHP
 
         // Reading the files with appropriate deliminators
-        
-        $lines = file("last_names.txt");
-        $f_name = $lines[array_rand($lines)];
-        echo $f_name;
 
-        $file = file_get_contents("first_names.csv");
-        $last_name = explode(",", $file);
-        $l_name = $last_name[array_rand($last_name)];
-        echo $l_name;
+        $l_name = file("last_names.txt");
+        $last_name = $l_name[array_rand($l_name)];
+
+        $f_name = file_get_contents("first_names.csv");
+        $firstname = explode(",", $f_name);
+        $first_name = $firstname[array_rand($firstname)];
 
         $sname = file_get_contents("street_names.txt");
         $street_n = explode(":", $sname);
-        echo $street_n[array_rand($street_n)];
+        $street_name = $street_n[array_rand($street_n)];
 
         $stype = file_get_contents("street_types.txt");
         $street_t = explode(";", $stype);
-        echo $street_t[array_rand($street_t)];
+        $street_type = $street_t[array_rand($street_t)];
+        
 
-        $email = $l_name . "." . $f_name;
-        echo $email;
-
+        $email = $last_name . "." . $first_name;
+        $domain = file_get_contents("domains.txt");
+        $array = preg_split('/(?<=.[a-z]{3}\b)./', $domain);
+        $domain_array = [];
+        for($j = 0; $j < 15 ; $j+=2){
+                $dom = $array[$j] . "." . $array[$j+1];
+                array_push($domain_array, $dom);
+        }
+        
+        $full_domain = $email . "@" . $domain_array[array_rand($domain_array)];
+        echo $full_domain;
 
     ?>
       
